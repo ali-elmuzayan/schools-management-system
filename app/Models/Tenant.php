1a7401c7821463072eb13b564569708dac2eec9a
ali-elmuzayan <ali.elmuzayan@gmail.com>
2025-07-02 01:02:03 +0300

    Tenant Model and start working on the tenatable trait for database isolation

---
+++ app/Models/Tenant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    /** @use HasFactory<\Database\Factories\TenantFactory> */
+    use HasFactory;
+
+    // public function school() {
+    //     return $this->hasMany(School::class)
+    // }
+}
---
+++ app/Tenantable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use App\Models\Scopes\TenantScope;
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+trait Tenantable
+{
+
+    public function tenant() {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public static function tenantBooted() {
+
+        static::addGlobalScope(new TenantScope);
+
+        static::creating(function (Model $model){
+    		$model['tenant_id'] =  Auth::user()->tenant_id;
+
+        });
+    }
+}
